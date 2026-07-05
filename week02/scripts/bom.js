f3cab047e7812dd2f7659e834557c4519bcdf310
jjones9825 <julie.jones007@outlook.com>
2026-07-05 14:57:32 -0600

    Week02 BOM JavaScript updates - Event Listeners

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,13 +2,44 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('ul');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
+const chapters = [];
 
-li.textContent = input.value;
 
-deleteButton.textContent = '❌';
+button.addEventListener('click', function(){
+    const chapter = input.value.trim();
+    
+    if (chapter !== ''){
+        if(chapters.length >= 10){
+            alert("You are only able to add 10 favorite chapters");
+            input.value = '';
+            input.focus();
+            return;
+        }
 
-li.append(deleteButton);
+        const li = document.createElement('li');
+        li.textContent = input.value;
+       
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        deleteButton.addEventListener('click', function(){
+            list.removeChild(li);
+            input.focus();
+        });
 
-list.append(li);
+        li.append(deleteButton);
+        list.append(li);
+        input.value = '';
+
+        chapters.push(chapter);
+    }
+    
+    else {
+        input.focus();
+    }
+    
+    input.focus();
+} );
+
+
+
+
