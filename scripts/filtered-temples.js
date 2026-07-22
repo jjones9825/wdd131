e521cacf852a61b06f6162c40ede8c8fcde0c294
jjones9825 <julie.jones007@outlook.com>
2026-07-21 18:19:13 -0700

    Working to get performance fixed - downloaded images and converted to webp, also changed javascript image URL

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -15,7 +15,7 @@ const temples = [
     dedicated: "2005, August, 7",
     area: 11500,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    "images/aba-nigeria-temple.webp"
   },
   {
     templeName: "Manti Utah",
@@ -23,7 +23,7 @@ const temples = [
     dedicated: "1888, May, 21",
     area: 74792,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    "images/manti-temple.webp"
   },
   {
     templeName: "Payson Utah",
@@ -31,7 +31,7 @@ const temples = [
     dedicated: "2015, June, 7",
     area: 96630,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    "images/payson-utah-temple.webp"
   },
   {
     templeName: "Yigo Guam",
@@ -39,7 +39,7 @@ const temples = [
     dedicated: "2020, May, 2",
     area: 6861,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    "images/yigo-guam-temple.webp"
   },
   {
     templeName: "Washington D.C.",
@@ -47,7 +47,7 @@ const temples = [
     dedicated: "1974, November, 19",
     area: 156558,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    "images/washington-dc-temple.webp"
   },
   {
     templeName: "Lima Perú",
@@ -55,7 +55,7 @@ const temples = [
     dedicated: "1986, January, 10",
     area: 9600,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    "images/lima-peru-temple.webp"
   },
   {
     templeName: "Mexico City Mexico",
@@ -63,7 +63,7 @@ const temples = [
     dedicated: "1983, December, 2",
     area: 116642,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    "images/mexico-city-temple.webp"
   },
   // my added temples
   {
@@ -72,7 +72,7 @@ const temples = [
     dedicated: "2000, March, 5",
     area: 34245,
     imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/albuquerque-new-mexico-temple/albuquerque-new-mexico-temple-56335-main.jpg"
+    "images/albuquerque-new-mexico-temple.webp"
   },
   {
     templeName: "Gila Valley Arizona",
@@ -80,7 +80,7 @@ const temples = [
     dedicated: "2010, May, 23",
     area: 18561,
     imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/the-gila-valley-arizona-temple/the-gila-valley-arizona-temple-12777-main.jpg"
+    "images/the-gila-valley-arizona-temple.webp"
   },
   {
     templeName: "Portland Oregon",
@@ -88,7 +88,7 @@ const temples = [
     dedicated: "1989, August, 21",
     area: 80500,
     imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/portland-oregon-temple/portland-oregon-temple-1629-main.jpg"
+    "images/portland-oregon-temple.webp"
   },
   {
     templeName: "Gilbert Arizona",
@@ -96,7 +96,7 @@ const temples = [
     dedicated: "2014, March, 2",
     area: 85326,
     imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/gilbert-arizona-temple/gilbert-arizona-temple-3802-main.jpg"
+    "images/gilbert-arizona-temple.webp"
   },
   {
     templeName: "Paris France",
@@ -104,7 +104,7 @@ const temples = [
     dedicated: "2017, May, 21",
     area: 44175,
     imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/paris-france-temple/paris-france-temple-2056-main.jpg"
+    "images/paris-france-temple.webp"
   }
 ];
 
